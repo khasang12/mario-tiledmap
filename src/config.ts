3a01c9925ba8e6b1f97d191935e3d2e557cb8ea7
Kha Sang <sang.khask12@hcmut.edu.vn>
2023-07-04 18:29:32 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+import { BootScene } from "./scenes/boot-scene";
+import { GameScene } from "./scenes/game-scene";
+import { HUDScene } from "./scenes/hud-scene";
+import { MenuScene } from "./scenes/menu-scene";
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: "Super Mario Land",
+  url: "https://github.com/digitsensitive/phaser3-typescript",
+  version: "2.0",
+  width: 320,
+  height: 288,
+  zoom: 5,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: [BootScene, MenuScene, HUDScene, GameScene],
+  input: {
+    keyboard: true,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 475 },
+      debug: true,
+    },
+  },
+  backgroundColor: "#f8f8f8",
+  render: { pixelArt: true, antialias: false },
+};
